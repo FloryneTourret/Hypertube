d1b4cf84bcb3865ea77f538660fddc76c2f43e20
Nathan Plouvier <nathanplouvier60@gmail.com>
2019-06-19 20:15:56 +0200

    streaming file start, github oauth wip

---
+++ client/src/views/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-white">Video : </h1>
+      <video width="100%" controls>
+        <source :src="src" type="video/mp4">
+      </video>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Player",
+  data() {
+    return {
+      src: "https://localhost:5001/api/v1/movies/stream?hash=" + this.$router.currentRoute.query.hash
+    }
+  },
+  mounted() {
+    console.log("mounted");
+  }
+};
+</script>
